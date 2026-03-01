a04324b6c9971c583bfdf274fd0bf33f128425fc
mint_duck <88096038+lilunderDuck@users.noreply.github.com>
2026-03-02 00:30:39 +0700

    Delete build/find_the_fucking_folder.ts
    should not have added that...

--- build/find_the_fucking_folder.ts
+++
@@ -1,9 +0,0 @@
-import { readdir, readdirSync, readFileSync, writeFileSync } from "node:fs"
-
-const data = []
-
-for (const dir of readdirSync("./build/bin/data/video_player/gallery/momokuri (manga)")) {
-  data.push({ fileName: dir })
-}
-
-writeFileSync("a.json", JSON.stringify(data))
